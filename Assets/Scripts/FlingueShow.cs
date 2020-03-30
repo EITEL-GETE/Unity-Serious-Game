3b8075f733d3c224474aa1e6c3ec90309c22abf8
Lag <f.gete@ludus-academie.com>
2020-03-30 15:33:45 +0200

    Inventaire

---
+++ Assets/Scripts/FlingueShow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlingueShow : MonoBehaviour
+{
+    public float velocity = 1.0f;
+
+    void Update()
+    {
+        gameObject.transform.eulerAngles += new Vector3(0, Time.deltaTime * velocity, 0);
+    }
+}
---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Hand : MonoBehaviour
+{
+    public GameObject weaponText;
+    public int nWeapon = 2;
+
+    public int weapon = 1;
+
+    void Update()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if (weapon < nWeapon)
+            {
+                weapon++;
+            }
+            else
+            {
+                weapon = 1;
+            }
+        }
+
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if (weapon > 1)
+            {
+                weapon--;
+            }
+            else
+            {
+                weapon = nWeapon;
+            }
+        }
+
+        // Choix de l'arme
+
+        switch (weapon)
+        {
+            case 1:
+                weaponText.GetComponent<UnityEngine.UI.Text>().text = "Pistol";
+                break;
+            case 2:
+                weaponText.GetComponent<UnityEngine.UI.Text>().text = "Shotgun";
+                break;
+        }
+    }
+}
