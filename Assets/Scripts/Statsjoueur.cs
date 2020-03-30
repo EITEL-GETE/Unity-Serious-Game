133137d62c0413aa302da9d626fb549eb218f5d3
Hugo <h.eitel@ludus.academie.fr>
2020-03-30 18:56:34 +0200

    réparation des bugs de merge

---
+++ Assets/Scripts/Statsjoueur.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Statsjoueur : MonoBehaviour {
+    public Image vie;
+    public float life, maxlife;
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        life = maxlife;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vie.fillAmount = life / maxlife;
+    }
+}
